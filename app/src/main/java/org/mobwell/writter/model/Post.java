743e00cc43b83fae7e4eb2dc738ef06143102fcc
McCazorla <macarena.cazorla@gmail.com>
2015-03-16 22:02:43 +0100

    Commit Post functions

--- app/src/main/java/org/mobwell/writter/model/Post.java
+++ app/src/main/java/org/mobwell/writter/model/Post.java
@@ -1,5 +1,8 @@
 package org.mobwell.writter.model;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
 /**
  * Created by dsangui on 3/11/15.
  */
@@ -35,5 +38,38 @@ public class Post {
 
     public void setTag(Hashtag tag) {
         this.tag = tag;
+    }
+
+
+    /*Funciones lógicas*/
+
+    public boolean guardarPost(String paragraph, Hashtag tag){
+        /*Guarda el Post en BBDD*/
+        return true;
+    }
+
+    public boolean eliminarPost(String date){
+        /*Elimina el Post de BBDD*/
+        return true;
+    }
+
+    public boolean modificarPost(String paragraph, Hashtag tag, String date){
+        /*Guarda el Post en BBDD y elimina el antiguo*/
+        if (guardarPost(paragraph,tag) && eliminarPost(date))return true;
+        else return false;
+    }
+
+    public ArrayList<Post> postActuales(){
+        /*Devuelve desde BBDD una lista de Post ordenados por fecha más actual*/
+        ArrayList<Post> actualizados = new ArrayList<Post>();
+        return actualizados;
+    }
+
+    public Post buscarPost (String date){
+        /*Devuelve desde BBDD un Post buscado mediante la fecha*/
+        String p = getParagraph();
+        Hashtag ht = getTag();
+        Post post = new Post(p, date, ht);
+        return post;
     }
 }
