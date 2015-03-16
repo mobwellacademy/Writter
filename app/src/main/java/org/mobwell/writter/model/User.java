18deadc9aa46a788b580693fc71e7ab03e89daff
Gaeawolf <antonio_rivas_33@hotmail.com>
2015-03-16 22:33:07 +0100

    subir

--- app/src/main/java/org/mobwell/writter/model/User.java
+++ app/src/main/java/org/mobwell/writter/model/User.java
@@ -15,6 +15,8 @@ public class User {
         this.foto = foto;
     }
 
+    public User(){}
+
     public String getNick() {
         return nick;
     }
@@ -46,4 +48,71 @@ public class User {
     public void setFoto(String foto) {
         this.foto = foto;
     }
+
+    //comprobar usuario (boolean (user, pass))
+    public boolean Login(String nick, String pass)
+    {
+        //Si el nick de la BD y su pass se corresponden
+        return true;
+        //si no
+        //return false;
+
+    }
+    //guardar/registrar (boolean o class (user,pass, email,desc,foto)
+    public boolean Register(String nick, String email, String descripcion, String foto, String pass)
+    {
+        /*if((nick ya existe en DB) || (nick no válido))return false; //usuario ya registrado o no válido
+
+        if((mail ya existe en DB) || (mail no válido)) return false; //mail ya registrado o no válido
+
+        if(pass no válida) //un mínimo de seguridad
+
+        if (descripcion no válida) return false; //por muchos y diversos motivos
+
+        if(!foto || !descripción) return false; //nada de campos vacíos
+
+        (Si funciona, subir a base de datos y)*/
+        return true;
+
+    }
+
+    public boolean ChangePass(String pass, String newpass, String npcheck)
+    {
+        /*if(pass==passdeDB) //comprobar pass
+        {
+            if ((newpass == npcheck) && (newpass es válida)) //seguridad redundante
+            {
+                //Grabar en base de datos nueva pass
+                return true;
+            }
+        }*/
+        return false; //con mensajito de error
+    }
+
+    public boolean ChangeFoto(String pass, String newfoto)
+    {
+        /*if(pass==passdeBD) //comprobar pass
+        {
+            if (newfoto es válida) //motivos obvios y legales
+            {
+                //Grabar foto en DB
+                return true;
+            }
+        }*/
+        return false; //con mensajito de error
+    }
+
+    public boolean ChangeDescription(String pass, String newdesc)
+    {
+        /*if(pass==passdeBD) //comprobar pass
+        {
+            if (newdesc es válida) //motivos obvios y legales
+            {
+                //Grabar descripcion en DB
+                return true;
+            }
+        }*/
+        return false; //con mensajito de error
+    }
+
 }
