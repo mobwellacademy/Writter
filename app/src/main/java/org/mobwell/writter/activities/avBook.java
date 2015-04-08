0918601eb860c4c2c9bc0430cf25379d0441fe2e
chemagg <chemagg@gmail.com>
2015-04-08 23:33:03 +0200

    Commit

--- app/src/main/java/org/mobwell/writter/activities/avBook.java
+++ app/src/main/java/org/mobwell/writter/activities/avBook.java
@@ -28,7 +28,7 @@ public class avBook extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        int id = item.getItemId();/*hola*/
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
